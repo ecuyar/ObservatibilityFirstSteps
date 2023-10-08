58cf1253d9c71b898b3020931502b696c5c90123
ecuyar <uyar.enescan@gmail.com>
2023-10-08 18:01:32 +0300

    - GlobalUsigns file is added - ActivitySourceProvider is added - OpenTelemetry constants are added

---
+++ ObservatibilityFirstSteps/ActivitySourceProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace ObservatibilityFirstSteps
+{
+	//ActivitySourceProvider must be created once. So make it static or Singleton
+	//Any activity that is created with ActivitySourceProvider will be watched from OpenTelemetry. 
+	internal static class ActivitySourceProvider
+	{
+		public static ActivitySource Source = new(OpenTelemetryConstants.ActivitySourceName);
+	}
+}
--- ObservatibilityFirstSteps/Program.cs
+++ ObservatibilityFirstSteps/Program.cs
@@ -1,1 +1,15 @@
 ﻿Console.WriteLine("Hello, World!");
+
+//create OpenTelemetry service
+var traceProvider = Sdk.CreateTracerProviderBuilder()
+	.AddSource(OpenTelemetryConstants.ActivitySourceName)
+	.ConfigureResource(config =>
+	{
+		config
+		.AddService(OpenTelemetryConstants.ServiceName, OpenTelemetryConstants.ServiceVersion)
+		.AddAttributes(new List<KeyValuePair<string, object>>()
+		{
+			new KeyValuePair<string, object>("host.machineName", Environment.MachineName),
+			 new KeyValuePair<string, object>("host.environment", "dev")
+		});
+	}).Build();
