9a5dc6e334b773eee60c25f7554971b638ff8a1c
Enes Can UYAR <uyar.enescan@gmail.com>
2023-10-08 18:28:38 +0300

    Confirmed
    - GlobalUsigns file is added
    - ActivitySourceProvider is added
    - OpenTelemetry constants are added

---
+++ ObservatibilityFirstSteps/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using ObservatibilityFirstSteps;
+global using OpenTelemetry;
+global using OpenTelemetry.Resources;
+global using OpenTelemetry.Trace;
+global using System.Diagnostics;
---
+++ ObservatibilityFirstSteps/OpenTelemetryConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ObservatibilityFirstSteps
+{
+	internal static class OpenTelemetryConstants
+	{
+		internal const string ServiceName = "TelemetryCompany.ConsoleApp.ConsoleService"; //CompanyName.AppName.ComponentName
+		internal const string ServiceVersion = "1.0.0";
+		internal const string ActivitySourceName = "ActivitySource.ConsoleApp";
+	}
+}
