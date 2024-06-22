0a09fb6d6ed2a32518bc49dfcc6e416a2708dcad
Enes Can UYAR <uyar.enescan@gmail.com>
2024-06-22 18:44:34 +0300

    Only controller based trace data will be collected, not all http requests including system events. (#11)

--- OrderAPI/Controllers/WeatherForecastController.cs
+++ OrderAPI/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace OrderAPI.Controllers
 {
 	[ApiController]
-	[Route("[controller]")]
+	[Route("api/[controller]")] //when we create an API controller, it comes with api prefix
 	public class WeatherForecastController : ControllerBase
 	{
 		private static readonly string[] Summaries = new[]
