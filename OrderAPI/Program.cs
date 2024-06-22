1109a20c0efb8c2823a83e97d58d355b847f5296
ecuyar <uyar.enescan@gmail.com>
2024-06-22 16:57:20 +0300

    - OpenTelemetry settings are created in appsettings - OpenTelemetry props are set in Program.cs - ActivitySourceProvider created

--- OrderAPI/Program.cs
+++ OrderAPI/Program.cs
@@ -1,11 +1,39 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OrderAPI.OpenTelemetry;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<OpenTelemetryConsts>(builder.Configuration.GetSection("OpenTelemetry")); //register config data for OpenTelemetry
+var openTelemetryConsts = builder.Configuration.GetSection("OpenTelemetry").Get<OpenTelemetryConsts>();
+
+//add OpenTelemetry service
+builder.Services.AddOpenTelemetry().WithTracing(config =>
+{
+	config.AddSource(openTelemetryConsts!.ActivitySourceName)
+	.ConfigureResource(resource =>
+	{
+		resource.AddService(serviceName: openTelemetryConsts.ServiceName,
+							serviceVersion: openTelemetryConsts.ServiceVersion);
+	});
+
+	config.AddAspNetCoreInstrumentation(); //add instrumentation
+	config.AddConsoleExporter(); //add where to export data
+	config.AddOtlpExporter(); //add where to export data (Jaeger)
+});
+
+//init ActivitySourceProvider
+ActivitySourceProvider.Source = new(
+	name: openTelemetryConsts!.ActivitySourceName,
+	version: openTelemetryConsts.ServiceVersion
+);
 
 var app = builder.Build();
 
