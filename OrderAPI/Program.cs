502d63c9aa8dab09c10fcb444b70af874602a0b8
Enes Can UYAR <uyar.enescan@gmail.com>
2024-09-28 22:00:52 +0300

    Merge AddRequestResponseMiddleware into master (#13)

--- OrderAPI/Program.cs
+++ OrderAPI/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.IO;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using OrderAPI.Middlewares;
 using OrderAPI.OpenTelemetry;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<RecyclableMemoryStreamManager>();
 
 builder.Services.Configure<OpenTelemetryConsts>(builder.Configuration.GetSection("OpenTelemetry")); //register config data for OpenTelemetry
 var openTelemetryConsts = builder.Configuration.GetSection("OpenTelemetry").Get<OpenTelemetryConsts>();
@@ -54,7 +58,11 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
+//built-in middleware
 app.UseHttpsRedirection();
+
+//custom middleware
+app.UseReadResponseMiddleware();
 
 app.UseAuthorization();
 
