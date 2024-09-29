6326a25c906cae86850a48fb855de52ae44959b7
Enes Can UYAR <uyar.enescan@gmail.com>
2024-09-29 22:40:12 +0300

    Open Telemetry configuration is seperated from Program.cs. (#14)

--- OrderAPI/Program.cs
+++ OrderAPI/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.IO;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 using OrderAPI.Middlewares;
-using OrderAPI.OpenTelemetry;
+using OrderAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,41 +11,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddOpenTelemetyService(builder.Configuration);
 builder.Services.AddSingleton<RecyclableMemoryStreamManager>();
-
-builder.Services.Configure<OpenTelemetryConsts>(builder.Configuration.GetSection("OpenTelemetry")); //register config data for OpenTelemetry
-var openTelemetryConsts = builder.Configuration.GetSection("OpenTelemetry").Get<OpenTelemetryConsts>();
-
-//add OpenTelemetry service
-builder.Services.AddOpenTelemetry().WithTracing(config =>
-{
-	config.AddSource(openTelemetryConsts!.ActivitySourceName)
-	.ConfigureResource(resource =>
-	{
-		resource.AddService(serviceName: openTelemetryConsts.ServiceName,
-							serviceVersion: openTelemetryConsts.ServiceVersion);
-	});
-
-	//add instrumentation
-	config.AddAspNetCoreInstrumentation(opt =>
-	{
-		opt.Filter = context =>
-		{
-			//filter only controller based requests' trace data, not system based requests
-			var pathValue = context.Request.Path.Value;
-			return pathValue is not null && pathValue.Contains("api", StringComparison.InvariantCultureIgnoreCase);
-		};
-		opt.RecordException = true; //get detailed exception instead of just Exception.Message
-	});
-	config.AddConsoleExporter(); //add where to export data
-	config.AddOtlpExporter(); //add where to export data (Jaeger)
-});
-
-//init ActivitySourceProvider
-ActivitySourceProvider.Source = new(
-	name: openTelemetryConsts!.ActivitySourceName,
-	version: openTelemetryConsts.ServiceVersion
-);
 
 var app = builder.Build();
 
---
+++ OrderAPI/Services/OpenTelemetryService.cs
@@ -0,0 +1,52 @@
+﻿using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OrderAPI.OpenTelemetry;
+
+namespace OrderAPI.Services
+{
+	public static class OpenTelemetryService
+	{
+		private const string OPEN_TELEMETRY = "OpenTelemetry";
+
+		public static IServiceCollection AddOpenTelemetyService(this IServiceCollection services, IConfiguration configuration)
+		{
+
+			services.Configure<OpenTelemetryConsts>(configuration.GetSection(OPEN_TELEMETRY)); //register config data for OpenTelemetry
+			var openTelemetryConsts = configuration.GetSection(OPEN_TELEMETRY).Get<OpenTelemetryConsts>();
+
+			//add OpenTelemetry service
+			services.AddOpenTelemetry().WithTracing(config =>
+			{
+				config.AddSource(openTelemetryConsts!.ActivitySourceName)
+					.ConfigureResource(resource =>
+					{
+						resource.AddService(
+							serviceName: openTelemetryConsts.ServiceName,
+							serviceVersion: openTelemetryConsts.ServiceVersion);
+					});
+
+				//add instrumentation
+				config.AddAspNetCoreInstrumentation(options =>
+				{
+					options.Filter = context =>
+					{
+						//filter only controller based requests' trace data, not system based requests
+						var pathValue = context.Request.Path.Value;
+						return pathValue is not null && pathValue.Contains("api", StringComparison.InvariantCultureIgnoreCase);
+					};
+					options.RecordException = true;  //get detailed exception instead of just Exception.Message
+				});
+				config.AddConsoleExporter(); //add where to export data
+				config.AddOtlpExporter(); //add where to export data (Jaeger)
+			});
+
+			//init ActivitySourceProvider
+			ActivitySourceProvider.Source = new(
+				name: openTelemetryConsts!.ActivitySourceName,
+				version: openTelemetryConsts!.ServiceVersion
+				);
+
+			return services;
+		}
+	}
+}
