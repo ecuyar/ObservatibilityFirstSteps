35cb2245a92d40a5f8435b471ff50fa3b1bcf112
ecuyar <uyar.enescan@gmail.com>
2025-03-15 21:52:00 +0300

    Update packages. Create StockAPI project for Http instrumentation.

---
+++ StockAPI/Controllers/StockController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockAPI.Controllers
+{
+	public class StockController : ControllerBase
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ StockAPI/StockService/StockService.cs
@@ -0,0 +1,22 @@
+﻿namespace StockAPI.StockService
+{
+	public class StockService
+	{
+		private static Dictionary<int, int> GetProductStockList()
+		{
+			Dictionary<int, int> productList = [];
+			productList[0] = 10;
+			productList[1] = 25;
+			productList[2] = 30;
+			productList[3] = 43;
+			return productList;
+		}
+
+		public async Task CheckAndPaymentService(CheckAndPaymentServiceRequestDto requestDto)
+		{
+			var productStocklist = GetProductStockList();
+			var stockStatus = new List<(int productId, bool isStockExists)>();
+
+		}
+	}
+}
