9a947cdea9c36c05f50199b273aea8b287c1d576
ecuyar <uyar.enescan@gmail.com>
2025-08-09 18:41:51 +0300

    Payment API created. Stock service calls payment service now. Related changes are done.

---
+++ Common.Shared/Dtos/PaymentCreateResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Shared.Dtos
+{
+	public record PaymentCreateResponseDto
+	{
+		public string Description { get; set; } = string.Empty;
+	}
+}
--- OrderAPI/StockServices/StockService.cs
+++ OrderAPI/StockServices/StockService.cs
@@ -8,7 +8,7 @@ public class StockService(HttpClient client)
 
 	public async Task<(bool isSuccess, List<string>? failMessage)> CheckAndPaymentAsync(CheckAndPaymentServiceRequestDto requestDto)
 	{
-		var response = await client.PostAsJsonAsync<CheckAndPaymentServiceRequestDto>(CHECK_AND_PAYMENT_ENDPOINT, requestDto);
+		var response = await client.PostAsJsonAsync(CHECK_AND_PAYMENT_ENDPOINT, requestDto);
 		var content = await response.Content.ReadFromJsonAsync<ResponseDto<CheckAndPaymentServiceResponseDto>>();
 
 		return response.IsSuccessStatusCode ? (true, null) : (false, content?.Errors);
